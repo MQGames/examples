da1e1d9f5ef92552b2d0d5964e05edb478fe0366
Peter Reeves <peter.reeves@mq.edu.au>
2019-02-23 01:38:34 +1100

    Finished colourful worms example.

--- 02.0_worm_drawing/js/main.js
+++ 02.0_worm_drawing/js/main.js
@@ -1,30 +1,58 @@
 
-// Heavily inspired by:
-// https://webglfundamentals.org/webgl/lessons/webgl-fundamentals.html
-// https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial
+/*
 
-// The "use strict"; part below will enable some extra checks by the browser.You almost always want to enable this!
-// It needs to be the first statement of code in order to work. For more info see:
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
+Click and drag to create colourful worms!
+
+*/
+
 "use strict";
 
 const VERTEX_SHADER_SOURCE = `
-	attribute vec4 aPosition;
+	attribute vec2 a_position;
+	attribute float a_size;
+	attribute vec3 a_colour;
+
+	uniform vec2 u_screenDimensions;
+
+	varying vec3 v_colour;
+
+	vec2 screenToNdc (vec2 screen, vec2 screenDimensions) {
+		return vec2(screen.x / (screenDimensions.x * 0.5) - 1.0, (screen.y / (screenDimensions.y * 0.5) - 1.0) * -1.0);
+	}
 
 	void main() {
-		gl_Position = aPosition;
+		v_colour = a_colour;
+		vec2 ndc = screenToNdc(a_position.xy, u_screenDimensions);
+		gl_Position = vec4(ndc, 0.0, 1.0);
+		gl_PointSize = a_size;
 	}
 `;
 
 const FRAGMENT_SHADER_SOURCE = `
 	precision mediump float;
 
+	varying vec3 v_colour;
+
 	void main() {
-		gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+		gl_FragColor = vec4(v_colour, 1.0);
 	}
 `;
 
-const points_buffer = new Float32Array();
+const MAX_POINTS = 10000;
+const POSITIONS_SIZE = 2;
+const SIZES_SIZE = 1;
+const COLOURS_SIZE = 3;
+const positionsArray = new Float32Array(MAX_POINTS * POSITIONS_SIZE);
+const sizesArray = new Float32Array(MAX_POINTS * SIZES_SIZE);
+const coloursArray = new Float32Array(MAX_POINTS * COLOURS_SIZE);
+const colourSpeed = 0.01;
+
+let currentPoints = 0;
+let currentPointIndex = 0;
+let mousePressed = false;
+let lastMouseX = 0;
+let lastMouseY = 0;
+let colourT = 0.0;
 
 function start () {
 	// Setup context
@@ -35,50 +63,111 @@ function start () {
 		return;
 	}
 
-	// Setup shaders
-	const vertexShader = createShader(gl, gl.VERTEX_SHADER, VERTEX_SHADER_SOURCE);
-	const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, FRAGMENT_SHADER_SOURCE);
-	const program = createProgram(gl, vertexShader, fragmentShader);
-	const positionAttributeLocation = gl.getAttribLocation(program, "aPosition");
+	// User interaction
+	{
+		canvas.addEventListener("mousedown", function (event) {
+			mousePressed = true;
+			lastMouseX = event.x;
+			lastMouseY = event.y;
+		});
+		canvas.addEventListener("mouseup", function () {
+			mousePressed = false;
+		});
+		canvas.addEventListener("mousemove", function (event) {
+			if (mousePressed) {
+				// Update buffers
+				{
+					positionsArray[currentPointIndex * POSITIONS_SIZE + 0] = event.x;
+					positionsArray[currentPointIndex * POSITIONS_SIZE + 1] = event.y;
 
-	// Setup vertex position buffer
-	const positions = [
-		0.0, 0.0,
-		0.8, 0.0,
-		0.0, 0.4,
-	];
+					gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+					gl.bufferSubData(gl.ARRAY_BUFFER, 0, positionsArray);
+
+					const manhattanDistance = Math.abs(event.x - lastMouseX) + Math.abs(event.y - lastMouseY);
+					sizesArray[currentPointIndex * SIZES_SIZE + 0] = manhattanDistance * 1.5;
+
+					gl.bindBuffer(gl.ARRAY_BUFFER, sizeBuffer);
+					gl.bufferSubData(gl.ARRAY_BUFFER, 0, sizesArray);
+
+					const colour = generateColour(colourT);
+					coloursArray[currentPointIndex * COLOURS_SIZE + 0] = colour.r;
+					coloursArray[currentPointIndex * COLOURS_SIZE + 1] = colour.g;
+					coloursArray[currentPointIndex * COLOURS_SIZE + 2] = colour.b;
+
+					gl.bindBuffer(gl.ARRAY_BUFFER, colourBuffer);
+					gl.bufferSubData(gl.ARRAY_BUFFER, 0, coloursArray);
+				}
+				
+				// Increment values
+				{
+					if (currentPoints < MAX_POINTS) {
+						currentPoints += 1;
+					}
+					currentPointIndex = (currentPointIndex + 1) % MAX_POINTS;
+
+					lastMouseX = event.x;
+					lastMouseY = event.y;
+
+					colourT = (colourT + colourSpeed) % 1.0;
+				}
+			}
+		});
+	}
+
+	// Setup shaders
+	const program = Util.createProgram(gl, VERTEX_SHADER_SOURCE, FRAGMENT_SHADER_SOURCE);
+	const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+	const sizeAttributeLocation = gl.getAttribLocation(program, "a_size");
+	const colourAttributeLocation = gl.getAttribLocation(program, "a_colour");
+	const screenDimensionsLocation = gl.getUniformLocation(program, "u_screenDimensions");
+
+	// Setup buffers
 	const positionBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+	gl.bufferData(gl.ARRAY_BUFFER, positionsArray, gl.DYNAMIC_DRAW);
+	const sizeBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, sizeBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, sizesArray, gl.DYNAMIC_DRAW);
+	const colourBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, colourBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, coloursArray, gl.DYNAMIC_DRAW);
 
 	// Specify which shader to use
 	gl.useProgram(program);
 
 	// Prepare position buffer
 	{
-		const size = 2;          // 2 components per iteration
 		const type = gl.FLOAT;   // the data is 32bit floats
 		const normalize = false; // don't normalize the data
 		const stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
 		const offset = 0;        // start at the beginning of the buffer
 		gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-		gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
+		gl.vertexAttribPointer(positionAttributeLocation, POSITIONS_SIZE, type, normalize, stride, offset);
 		gl.enableVertexAttribArray(positionAttributeLocation);
+
+		gl.bindBuffer(gl.ARRAY_BUFFER, sizeBuffer);
+		gl.vertexAttribPointer(sizeAttributeLocation, SIZES_SIZE, type, normalize, stride, offset);
+		gl.enableVertexAttribArray(sizeAttributeLocation);
+
+		gl.bindBuffer(gl.ARRAY_BUFFER, colourBuffer);
+		gl.vertexAttribPointer(colourAttributeLocation, COLOURS_SIZE, type, normalize, stride, offset);
+		gl.enableVertexAttribArray(colourAttributeLocation);
 	}
 
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
 	const render = function () {
-		resizeCanvas(canvas);
+		Util.resizeCanvas(canvas);
 		gl.viewport(0, 0, canvas.width, canvas.height);
 
 		gl.clear(gl.COLOR_BUFFER_BIT);
 
-		// Draw the triangle
-		{
-			const primitiveType = gl.TRIANGLES;
-			const offset = 0;
-			const count = 3;
+		gl.uniform2fv(screenDimensionsLocation, [canvas.width, canvas.height]);
+
+		const primitiveType = gl.POINTS;
+		const offset = 0;
+		const count = currentPoints;
+		if (currentPoints > 0) {
 			gl.drawArrays(primitiveType, offset, count);
 		}
 	};
@@ -91,54 +180,16 @@ function start () {
 	animate();
 }
 
-function resizeCanvas (canvas) {
-	check(isCanvas(canvas));
+// Taken from:
+// https://iquilezles.org/www/articles/palettes/palettes.htm
+function generateColour (t) {
+	const r = 0.5 + 0.5 * Math.cos(2 * Math.PI * (1.0 * t + 0.00));
+	const g = 0.5 + 0.5 * Math.cos(2 * Math.PI * (1.0 * t + 0.33));
+	const b = 0.5 + 0.5 * Math.cos(2 * Math.PI * (1.0 * t + 0.67));
 
-	const cssToRealPixels = window.devicePixelRatio || 1.0;
-
-	const displayWidth = Math.floor(canvas.clientWidth  * cssToRealPixels);
-	const displayHeight = Math.floor(canvas.clientHeight * cssToRealPixels);
-
-	if (canvas.width !== displayWidth || canvas.height !== displayHeight) {
-		canvas.width = canvas.clientWidth;
-		canvas.height = canvas.clientHeight;
-		return true;
-	}
-	else {
-		return false;
-	}
-}
-
-function createShader (gl, type, source) {
-	check(isContext(gl), isString(source));
-
-	const shader = gl.createShader(type);
-	gl.shaderSource(shader, source);
-	gl.compileShader(shader);
-	const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-	if (!success) {
-		console.error(gl.getShaderInfoLog(shader));
-		gl.deleteShader(shader);
-		return null;
-	}
-
-	return shader;
-}
-
-function createProgram (gl, vertexShader, fragmentShader) {
-	check(isContext(gl), isShader(vertexShader), isShader(fragmentShader));
-
-	const program = gl.createProgram();
-	gl.attachShader(program, vertexShader);
-	gl.attachShader(program, fragmentShader);
-	gl.linkProgram(program);
-
-	const success = gl.getProgramParameter(program, gl.LINK_STATUS);
-	if (!success) {
-		console.error(gl.getProgramInfoLog(program));
-		gl.deleteProgram(program);
-		return null;
-	}
-
-	return program;
+	return {
+		r: r,
+		g: g,
+		b: b,
+	};
 }
