c3be6c3fad43ef4eb1ba4fc83a106d33cae9fdd3
Peter Reeves <peter.reeves@mq.edu.au>
2019-04-12 15:04:22 +1000

    YOLO successful.

--- 07.0_3d_core/js/gameObject.js
+++ 07.0_3d_core/js/gameObject.js
@@ -1,8 +1,40 @@
 "use strict";
 
 class GameObject {
-    constructor () {
+    constructor (geometry) {
+        this.position = new Float32Array([0, 0, 0]);
+        this.rotation = new Float32Array([0, 0, 0]);
+        this.scale = new Float32Array([1, 1, 1]);
 
+        this.colour = new Float32Array([1, 0, 0]);
+
+        this.children = [];
+
+        this.geometry = geometry;
+    }
+
+    draw (gl, parentMatrix, uniforms) {
+        const local = glMatrix.mat4.create();
+        glMatrix.mat4.fromTranslation(local, this.position);
+        glMatrix.mat4.rotateX(local, local, this.rotation[0]);
+        glMatrix.mat4.rotateY(local, local, this.rotation[1]);
+        glMatrix.mat4.rotateZ(local, local, this.rotation[2]);
+        glMatrix.mat4.scale(local, local, this.scale);
+
+        glMatrix.mat4.multiply(local, parentMatrix, local);
+
+        gl.uniformMatrix4fv(uniforms.world, false, local);
+        gl.uniform3fv(uniforms.colour, this.colour);
+
+        const g = this.geometry;
+        gl.bindBuffer(gl.ARRAY_BUFFER, g.buffer);
+        gl.vertexAttribPointer(g.attribute, g.size, g.type, g.normalize, g.stride, g.offset);
+        gl.drawArrays(g.primitive, g.start, g.numPoints);
+
+        for (let i = 0; i < this.children.length; i++) {
+            const child = this.children[i];
+            child.draw(gl, local, uniforms);
+        }
     }
 }
 const isGameObject = newCheck(function (g) {
--- 07.0_3d_core/js/main.js
+++ 07.0_3d_core/js/main.js
@@ -1,8 +1,26 @@
 "use strict";
 
-const VERTEX_SHADER_SOURCE = ``;
+const VERTEX_SHADER_SOURCE = `
+    attribute vec3 a_position;
 
-const FRAGMENT_SHADER_SOURCE = ``;
+    uniform mat4 u_world;
+    uniform mat4 u_camera;
+    uniform mat4 u_projection;
+
+    void main () {
+        gl_Position = u_projection * u_camera * u_world * vec4(a_position, 1.0);
+    }
+`;
+
+const FRAGMENT_SHADER_SOURCE = `
+    precision highp float;
+
+    uniform vec3 u_colour;
+
+    void main () {
+        gl_FragColor = vec4(u_colour, 1.0);
+    }
+`;
 
 function start () {
     // Setup context
@@ -13,7 +31,133 @@ function start () {
         return;
     }
 
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(0, 0, 0, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    const program = Util.createProgram(gl, VERTEX_SHADER_SOURCE, FRAGMENT_SHADER_SOURCE);
+    gl.useProgram(program);
+
+    const postionAttribute = gl.getAttribLocation(program, "a_position");
+    const worldUniform = gl.getUniformLocation(program, "u_world");
+    const cameraUniform = gl.getUniformLocation(program, "u_camera");
+    const projectionUniform = gl.getUniformLocation(program, "u_projection");
+    const colourUniform = gl.getUniformLocation(program, "u_colour");
+
+    gl.enableVertexAttribArray(postionAttribute);
+
+    const squarePositions = new Float32Array([
+        -1, 0, -1,
+        -1, 0,  1,
+
+        -1, 0,  1,
+         1, 0,  1,
+
+         1, 0,  1,
+         1, 0, -1,
+
+         1, 0, -1,
+        -1, 0, -1,
+    ]);
+    const squareBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, squarePositions, gl.STATIC_DRAW);
+
+    const uniforms = {
+        world: worldUniform,
+        colour: colourUniform,
+    };
+
+    const geometry = {
+        buffer: squareBuffer,
+        start: 0,
+        numPoints: squarePositions.length / 3,
+        attribute: postionAttribute,
+        size: 3,
+        type: gl.FLOAT,
+        normalize: false,
+        stride: 0,
+        offset: 0,
+        primitive: gl.LINES,
+    };
+
+    const squareGameObject = new GameObject(geometry);
+    squareGameObject.colour[0] = 0;
+    squareGameObject.colour[1] = 1;
+    squareGameObject.colour[2] = 1;
+
+    const square2GameObject = new GameObject(geometry);
+    square2GameObject.colour[0] = 1;
+    square2GameObject.colour[1] = 0;
+    square2GameObject.colour[2] = 1;
+    square2GameObject.position[0] = 3;
+
+    squareGameObject.children.push(square2GameObject);
+
+    const render = function (t) {
+        Util.resizeCanvas(canvas);
+
+        const aspect = canvas.width / canvas.height;
+
+        const projection = glMatrix.mat4.create();
+        glMatrix.mat4.perspective(projection, Math.PI / 2.0, aspect, 0.1, 100.0);
+        gl.uniformMatrix4fv(projectionUniform, false, projection);
+        const camera = glMatrix.mat4.create();
+        glMatrix.mat4.lookAt(camera, [4, 2, 3], [0, 0, 0], [0, 1, 0]);
+        gl.uniformMatrix4fv(cameraUniform, false, camera);
+
+        gl.viewport(0, 0, canvas.width, canvas.height);
+        gl.clearColor(0, 0, 0, 1);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        squareGameObject.rotation[1] = t;
+        square2GameObject.rotation[1] = t;
+
+        squareGameObject.draw(gl, glMatrix.mat4.create(), uniforms);
+
+        /*
+        // Square
+        {
+            const world = glMatrix.mat4.create();
+            gl.uniformMatrix4fv(worldUniform, false, world);
+
+            gl.uniform3fv(colourUniform, [1, 0, 1]);
+
+            gl.bindBuffer(gl.ARRAY_BUFFER, squareBuffer);
+            gl.vertexAttribPointer(postionAttribute, 3, gl.FLOAT, false, 0, 0);
+            gl.drawArrays(gl.LINES, 0, squarePositions.length / 3);
+        }
+
+        // Square
+        {
+            const world = glMatrix.mat4.create();
+            glMatrix.mat4.fromTranslation(world, [2, 0, 0]);
+            gl.uniformMatrix4fv(worldUniform, false, world);
+
+            gl.uniform3fv(colourUniform, [1, 0, 0]);
+
+            gl.bindBuffer(gl.ARRAY_BUFFER, squareBuffer);
+            gl.vertexAttribPointer(postionAttribute, 3, gl.FLOAT, false, 0, 0);
+            gl.drawArrays(gl.LINES, 0, squarePositions.length / 3);
+        }
+
+        // Square
+        {
+            const world = glMatrix.mat4.create();
+            glMatrix.mat4.fromTranslation(world, [0, 0, 2]);
+            gl.uniformMatrix4fv(worldUniform, false, world);
+
+            gl.uniform3fv(colourUniform, [0, 1, 0]);
+
+            gl.bindBuffer(gl.ARRAY_BUFFER, squareBuffer);
+            gl.vertexAttribPointer(postionAttribute, 3, gl.FLOAT, false, 0, 0);
+            gl.drawArrays(gl.LINES, 0, squarePositions.length / 3);
+        }
+        */
+    };
+
+    const animate = function (milliseconds) {
+        window.requestAnimationFrame(animate);
+
+        const seconds = milliseconds / 1000;
+        render(seconds);
+    };
+
+    animate(0);
 }
